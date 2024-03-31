dd366859516111c810393e5607dc34f6ef06164d
deceitya <49361292+deceitya@users.noreply.github.com>
2024-03-31 18:36:53 +0900

    internalにした

--- src/main/kotlin/mc/tsukimiya/namelogger/presentation/listener/PlayerJoinListener.kt
+++ src/main/kotlin/mc/tsukimiya/namelogger/presentation/listener/PlayerJoinListener.kt
@@ -5,7 +5,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 
-class PlayerJoinListener : Listener {
+internal class PlayerJoinListener : Listener {
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         val player = event.player

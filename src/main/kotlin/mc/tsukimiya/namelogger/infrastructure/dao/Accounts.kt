4e2c3d051dda69f046128d65caada8be359070c7
deceitya <49361292+deceitya@users.noreply.github.com>
2024-04-05 01:59:15 +0900

    デフォルト値はいらない

--- src/main/kotlin/mc/tsukimiya/namelogger/infrastructure/dao/Accounts.kt
+++ src/main/kotlin/mc/tsukimiya/namelogger/infrastructure/dao/Accounts.kt
@@ -2,10 +2,9 @@ package mc.tsukimiya.namelogger.infrastructure.dao
 
 import org.jetbrains.exposed.dao.id.UUIDTable
 import org.jetbrains.exposed.sql.javatime.datetime
-import java.time.LocalDateTime
 
 object Accounts : UUIDTable("accounts") {
     val name = varchar("name", 16).uniqueIndex()
-    val createdAt = datetime("created_at").default(LocalDateTime.now())
-    val updatedAt = datetime("updated_at").default(LocalDateTime.now())
+    val createdAt = datetime("created_at")
+    val updatedAt = datetime("updated_at")
 }
